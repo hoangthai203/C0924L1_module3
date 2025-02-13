78a3c4c4e8a0cf16f7729ef74eba98c8194edfeb
hoangthai203 <hoangthai16122003@gmail.com>
2025-02-13 21:13:18 +0700

    nop bai

--- database/ss2/ss2_thiet_ke_csdl.sql
+++ database/ss2/ss2_thiet_ke_csdl.sql
@@ -1,70 +1,73 @@
 create database if not exists quan_li_kho;
 use quan_li_kho;
 
-create table vattu(
-mavattu varchar(20) primary key,
-tenvattu varchar(50)
+create table nha_cung_cap (
+    ma_nha_cung_cap int primary key,
+    ten_nha_cung_cap varchar(50),
+    dia_chi varchar(50)
 );
 
-create table phieuxuat(
-sophieuxuat int primary key auto_increment,
-ngayxuat date
+create table phieu_xuat (
+    ma_phieu int primary key,
+    ngay_xuat date
 );
 
-create table chitietphieuxuat(
-phieuxuat_sophieuxuat int,
-vattu_mavattu varchar(20),
-dongiaxuat int,
-soluongxuat int,
-primary key(phieuxuat_sophieuxuat,vattu_mavattu),
-foreign key (phieuxuat_sophieuxuat) references phieuxuat(sophieuxuat),
-foreign key (vattu_mavattu) references vattu(mavattu)
+create table vat_tu (
+    ma_vat_tu int primary key,
+    ten_vat_tu varchar(50)
 );
 
-create table phieunhap(
-sophieunhap int primary key auto_increment,
-ngaynhap date
+create table phieu_nhap (
+    ma_phieu int primary key,
+    ngay_nhap date
 );
 
-create table chitietphieunhap(
-phieunhap_sophieunhap int,
-vattu_mavattu varchar(20),
-dongianhap int,
-soluongnhap int,
-primary key(phieunhap_sophieunhap,vattu_mavattu),
-foreign key (phieunhap_sophieunhap) references phieunhap(sophieunhap),
-foreign key (vattu_mavattu) references vattu(mavattu)
+create table don_dat_hang (
+    ma_don_hang int primary key,
+    ngay_dat_hang date,
+    ma_nha_cung_cap int,
+    foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
 );
 
-create table dondathang(
-sodathang int primary key auto_increment,
-ngaydathang date
+create table so_dien_thoai_nha_cung_cap (
+    id int auto_increment primary key,
+    ma_nha_cung_cap int,
+    so_dien_thoai varchar(20),
+    foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
 );
 
-create table chitietdathang(
-dondathang_sodathang int,
-vattu_mavattu varchar(20),
-primary key(dondathang_sodathang,vattu_mavattu),
-foreign key (dondathang_sodathang) references dondathang(sodathang),
-foreign key (vattu_mavattu) references vattu(mavattu)
+create table chi_tiet_phieu_xuat (
+    id int auto_increment primary key,
+    ma_phieu int,
+    ma_vat_tu int,
+    don_gia_xuat decimal(10, 2),
+    so_luong_xuat int,
+    foreign key (ma_phieu) references phieu_xuat(ma_phieu),
+    foreign key (ma_vat_tu) references vat_tu(ma_vat_tu)
 );
 
-create table nhacungcap(
-manhacungcap varchar(20) primary key,
-tennhacungcap varchar(50),
-diachi varchar(50)
+create table chi_tiet_phieu_nhap (
+    id int auto_increment primary key,
+    ma_vat_tu int,
+    ma_phieu int,
+    don_gia_nhap decimal(10, 2),
+    so_luong_nhap int,
+    foreign key (ma_phieu) references phieu_nhap(ma_phieu),
+    foreign key (ma_vat_tu) references vat_tu(ma_vat_tu)
 );
 
-create table sdt(
-sdtnhacungcap int primary key,
-nhacungcap_manhacungcap varchar(20),
-foreign key (nhacungcap_manhacungcap) references nhacungcap(manhacungcap)
+create table chi_tiet_don_dat_hang (
+    id int auto_increment primary key,
+    ma_vat_tu int,
+    ma_don_hang int,
+    foreign key (ma_don_hang) references don_dat_hang(ma_don_hang),
+    foreign key (ma_vat_tu) references vat_tu(ma_vat_tu)
 );
 
-create table cungcap(
-dondathang_sodathang int,
-nhacungcap_manhacungcap varchar(20),
-primary key(dondathang_sodathang,nhacungcap_manhacungcap),
-foreign key (dondathang_sodathang) references dondathang(sodathang),
-foreign key (nhacungcap_manhacungcap) references nhacungcap(manhacungcap)
+create table cung_cap (
+    id int auto_increment primary key,
+    ma_don_hang int,
+    ma_nha_cung_cap int,
+    foreign key (ma_don_hang) references don_dat_hang(ma_don_hang),
+    foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
 );
