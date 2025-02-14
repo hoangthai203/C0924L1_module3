cad227f7c2ea417c23b0704afbbb4b5da7971d13
hoangthai203 <hoangthai16122003@gmail.com>
2025-02-14 19:45:55 +0700

    nop bai

---
+++ database/ss4/ham_thong_dung_qlsv.sql
@@ -0,0 +1,16 @@
+use quan_ly_sinh_vien;
+-- câu 1  Hiển thị tất cả các thông tin môn học có credit lớn nhất --
+select * from subject	
+where credit = (select max(credit) from subject);
+
+-- câu 2 Hiển thị các thông tin môn học có điểm thi lớn nhất --
+select sub.* from subject sub
+join mark m on sub.sub_id = m.sub_id
+where m.mark = (select max(mark) from mark);
+
+-- câu 3 Hiển thị thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần --
+select s.student_id, s.student_name, s.address, s.phone, avg(m.mark) as avg_score
+from student s
+join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name, s.address, s.phone
+order by avg_score desc;
